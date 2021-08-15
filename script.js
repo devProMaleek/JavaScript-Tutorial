393e7cd9cdd299d4c9e7db7303bfabff6424814f
devProMaleek <milikiadbay@gmail.com>
2021-08-15 19:35:23 +0100

    Learnt Javascript callback JavaScript Asynchronous

--- script.js
+++ script.js
@@ -131,35 +131,104 @@
 //Creating an Objects
 
 // declare global variable.
-const result = document.getElementById('results');
-const person = {firstName: "Abdulmalik", lastName: "Adebayo", age: 30, nickName: "devProMaleek", eyeCol0r: "black"};
-// console.log(result)
- //,Output Results
-//  output.innerHTML = "My name is " + person.firstName + " " + person.lastName  + ". You can also call me " + person.nickName; 
+// const result = document.getElementById('results');
+// const person = {firstName: "Abdulmalik", lastName: "Adebayo", age: 30, nickName: "devProMaleek", eyeCol0r: "black"};
+// // console.log(result)
+//  //,Output Results
+// //  output.innerHTML = "My name is " + person.firstName + " " + person.lastName  + ". You can also call me " + person.nickName; 
 
- // Add more property to an object.
+//  // Add more property to an object.
 
- person.level = 500;
- person.department = "Electronic and computer Engineering";
+//  person.level = 500;
+//  person.department = "Electronic and computer Engineering";
 
-//  output.innerHTML = person;
+// //  output.innerHTML = person;
 
- // Javascript Objects are mutable.
+//  // Javascript Objects are mutable.
 
- const x = person;
- x.age = 10; // This will change both x.age and person.age.
+//  const x = person;
+//  x.age = 10; // This will change both x.age and person.age.
 
- // Results
-result.innerHTML = person.age;
+//  // Results
+// result.innerHTML = person.age;
 
-// Displaying an Object
+// // Displaying an Object
 
-const myArray = Object.values(person);
+// const myArray = Object.values(person);
 
-result.innerHTML = myArray;
+// result.innerHTML = myArray;
 
 // Using JSON Stringify
 
-let myString = JSON.stringify(person);
+// let myString = JSON.stringify(person);
 
-result.innerHTML = myString;
+// result.innerHTML = myString;
+
+
+// JavaScript Callbacks
+
+// Sequence Control on Functions
+
+// Suppose you want to do a calculation, and then display the result.
+// You could call a calculator function (myCalculator), save the result, and then call another function (myDisplayer) to display the result:
+
+const output = document.getElementById("output")
+// function myDisplayer(some) {
+//     output.innerHTML = some;
+// }
+
+// function myCalculator(num1, num2) {
+//     let sum = num1 + num2;
+//     return sum;
+// }
+
+// let result = myCalculator(10, 5);
+// myDisplayer(result);
+
+// Callback Example.
+// function myDisplayer(some) {
+//     output.innerHTML = some;
+// }
+
+// function myCalculator(num1, num2, myCallback) {
+//     let sum = num1 + num2;
+//     myCallback(sum);
+// }
+
+// myCalculator(20, 10, myDisplayer);
+
+// Waiting for a timeout.
+
+// setTimeout(myFunction, 3000)
+
+// function myFunction() {
+//     output.innerHTML = "I fallen in love with JavaScript."
+// } 
+
+// Waiting for Intervals.
+// setInterval(myFunction, 1000)
+// function myFunction() {
+//     let d = new Date();
+//     output.innerHTML = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+// }
+
+// Waiting for files
+
+function myDisplayer(some) {
+    output.innerHTML = some;
+}
+
+function getFile(myCallback) {
+    let req = new XMLHttpRequest();
+    req.open('GET', "mycar.html");
+    req.onload = function() {
+        if (req.status == 200) {
+            myCallback(this.responseText);
+        } else {
+            myCallback("Error: " + req.status)
+        }
+     }
+     req.send();
+}
+
+getFile(myDisplayer);
